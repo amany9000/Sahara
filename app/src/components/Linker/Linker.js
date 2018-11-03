814b232dcca04170191b79ea3c113d52120a1c26
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-11-03 14:35:48 +0530

    Add linker for routing and update old settings

---
+++ app/src/components/Linker/Linker.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+  } from 'react-router-dom';
+import { List, Avatar, Button, Spin, Menu, Icon } from 'antd';
+
+import Projects from '../Pages/Projects/Projects.js';
+import Home from '../Pages/Home/Home.js';
+import AboutUs from '../Pages/AboutUs/AboutUs.js';
+import ContactUs from '../Pages/Contactus/Contactus.js';
+
+class Linker extends Component {
+  render() {
+    return (
+        <Router>
+        <div>
+            <Menu
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={["1"]}
+                style={{ lineHeight: '64px' }}
+                breakpoint="lg"
+                collapsedWidth="0"
+            >
+                <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
+                <Menu.Item key="2"><Link to="/projects">Project List</Link></Menu.Item>
+                <Menu.Item key="3"><Link to="/aboutus">About Us</Link></Menu.Item>
+                <Menu.Item key="4"><Link to="/contactus">Contact Us</Link></Menu.Item>
+                <span style={{
+                    marginRight: '20px',
+                    float: 'right'
+                }} >
+                <Icon style={{ fontSize: '20px', color: 'blue', margin: '10px' }} type="facebook" theme="outlined" />
+                <Icon style={{ fontSize: '20px', color: 'red', margin: '10px' }} type="youtube" theme="outlined" />
+                <Icon style={{ fontSize: '20px', color: '#08c', margin: '10px' }} type="twitter" theme="outlined" />
+                <Icon style={{ fontSize: '20px', color: 'pink', margin: '10px' }} type="instagram" theme="outlined" />
+                </span>
+            </Menu>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/projects" component={Projects}/>
+          <Route exact path="/contactus" component={ContactUs} />
+          <Route exact path="/aboutus" component={AboutUs} />
+        </div>
+    </Router>
+    );
+  }
+}
+
+export default Linker;
--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Linker from './components/Linker/Linker.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Linker />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
